e1286f6b25e5290e9a038d91b780ca8325f87bdc
daihanqiao <daihanqiao@126.com>
2022-09-05 22:39:54 +0800

    fix staking

--- src/panels/main/staking/index.js
+++ src/panels/main/staking/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Content, Spinner, View } from 'native-base';
-import { Nav, S, SS, ThemeVar } from '@/common';
+import { Nav, S, SS, ThemeVar, Toast } from '@/common';
 import { StatusCon, AirdopsList } from './widget';
 import { useGetEventsConfig } from '@tangle-pay/store/staking';
 import { useStore } from '@tangle-pay/store';
 import { I18n } from '@tangle-pay/common';
+import { useGetNodeWallet, useGetAssetsList } from '@tangle-pay/store/common';
+
 export const Staking = () => {
+	const [curWallet] = useGetNodeWallet();
+	useGetAssetsList(curWallet);
 	useGetEventsConfig();
 	const [isRequestStakeHis] = useStore('common.isRequestStakeHis');
+	useEffect(() => {
+		isRequestStakeHis ? Toast.hideLoading() : Toast.showLoading();
+	}, [isRequestStakeHis]);
 	return (
 		<Container>
 			<Nav title={I18n.t('staking.title')} />
@@ -15,11 +22,11 @@ export const Staking = () => {
 				<StatusCon />
 				<AirdopsList />
 			</Content>
-			{!isRequestStakeHis && (
+			{/* {!isRequestStakeHis && (
 				<View style={[SS.c, S.wh(ThemeVar.deviceWidth, ThemeVar.deviceHeight), SS.pa, { left: 0, top: 0 }]}>
 					<Spinner color='gray' />
 				</View>
-			)}
+			)} */}
 		</Container>
 	);
 };
