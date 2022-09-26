f9c38dac0fd103b4a75f711fee1afcf33ab28e67
daihanqiao <daihanqiao@126.com>
2022-09-27 02:34:55 +0800

    filter nft/alias/foundry

--- bin/template/iota-next-index-app.js
+++ bin/template/iota-next-index-app.js
@@ -3835,6 +3835,16 @@
 	 */
 	B1T6.TRITS_PER_TRYTE = 3;
 
+	// check output
+	async function checkOutput(output) {
+		const isSpent = output?.metadata?.isSpent;
+		const outputType = output?.output?.type;
+		const nativeTokens = output?.output?.nativeTokens || [];
+		let unlockConditions = output?.output?.unlockConditions || [];
+		const unlockConditionsData = unlockConditions.find((e) => e.type != ADDRESS_UNLOCK_CONDITION_TYPE);
+		return !isSpent && outputType == BASIC_OUTPUT_TYPE && !nativeTokens.length && !unlockConditionsData;
+	}
+
 	// Copyright 2020 IOTA Stiftung
 	/**
 	 * Get the balance for an address.
@@ -3859,11 +3869,7 @@
 				if (!output.metadata.isSpent) {
 					total = total.plus(output.output.amount);
 					const nativeTokenOutput = output.output?.nativeTokens || [];
-					const unlockConditions = output.output?.unlockConditions || [];
-					const addressUnlockCondition = unlockConditions.find(
-						(u) => u.type === EXPIRATION_UNLOCK_CONDITION_TYPE
-					);
-					if (!addressUnlockCondition && !nativeTokenOutput.length) {
+					if (checkOutput(output)) {
 						available = available.plus(output.output.amount);
 					}
 					if (nativeTokenOutput.length > 0) {
@@ -3903,12 +3909,7 @@
 			response = await indexerPluginClient.outputs({ addressBech32, cursor });
 			for (const outputId of response.items) {
 				const output = await localClient.output(outputId);
-				const unlockConditions = output.output?.unlockConditions;
-				const addressUnlockCondition = unlockConditions.find(
-					(u) => u.type === EXPIRATION_UNLOCK_CONDITION_TYPE
-				);
-				const nativeTokens = output.output?.nativeTokens || [];
-				if (!output.metadata.isSpent && !addressUnlockCondition && !nativeTokens.length) {
+				if (checkOutput(output)) {
 					total = total.plus(output.output.amount);
 					const nativeTokenOutput = output.output;
 					if (Array.isArray(nativeTokenOutput.nativeTokens)) {
@@ -4688,16 +4689,7 @@
 			} else {
 				for (const addressOutputId of addressOutputIds.items) {
 					const addressOutput = await localClient.output(addressOutputId);
-					const addressUnlockCondition = addressOutput.output.unlockConditions.find(
-						(u) => u.type === EXPIRATION_UNLOCK_CONDITION_TYPE
-					);
-					const nativeTokens = addressOutput.output?.nativeTokens || [];
-					if (
-						!addressOutput.metadata.isSpent &&
-						consumedBalance.lesser(requiredBalance) &&
-						!addressUnlockCondition &&
-						!nativeTokens.length
-					) {
+					if (checkOutput(addressOutput) && consumedBalance.lesser(requiredBalance)) {
 						if (bigInt__default['default'](addressOutput.output.amount).equals(0)) {
 							zeroBalance++;
 							if (zeroBalance >= zeroCount) {
@@ -5996,6 +5988,7 @@
 	exports.UINT8_SIZE = UINT8_SIZE;
 	exports.UTXO_INPUT_TYPE = UTXO_INPUT_TYPE;
 	exports.UnitsHelper = UnitsHelper;
+	exports.checkOutput = checkOutput;
 	exports.addressBalance = addressBalance;
 	exports.addressUnlockBalance = addressUnlockBalance;
 	exports.blockIdFromMilestonePayload = blockIdFromMilestonePayload;
