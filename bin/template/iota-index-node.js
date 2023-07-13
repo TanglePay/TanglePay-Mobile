2d2acae15edc16340b1e4024602f7cfa045f09cf
daihanqiao <daihanqiao@126.com>
2023-07-13 17:08:53 +0800

    fix error

--- bin/template/iota-index-node.js
+++ bin/template/iota-index-node.js
@@ -1485,6 +1485,7 @@
 		 * @param code The code in the payload.
 		 */
 		constructor(message, route, httpStatus, code) {
+			message = `route=${route}, ${message}`;
 			super(message);
 			this.route = route;
 			this.httpStatus = httpStatus;
@@ -1961,7 +1962,9 @@
 				});
 				return response;
 			} catch (err) {
-				throw err instanceof Error && err.name === 'AbortError' ? new Error('Timeout') : err;
+				throw err instanceof Error && err.name === 'AbortError'
+					? new Error('Timeout')
+					: `route=${route} ` + String(err);
 			} finally {
 				if (timerId) {
 					clearTimeout(timerId);
